137082f44a8ca36466d2eef86f5ffc5af9b73af7
Skenos Peniel <skenos3s@gmail.com>
2021-10-24 21:59:54 +0530

    Completed Project 3

--- src/main/java/com/example/emart/session/PasswordEncoder.java
+++ src/main/java/com/example/emart/session/PasswordEncoder.java
@@ -3,10 +3,10 @@ package com.example.emart.session;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordEncoder {
-    public static void main(String[] args) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String rawPassword = "12345678";
-        String encodedPassword = encoder.encode(rawPassword);
-        System.out.println(encodedPassword);
-    }
+//    public static void main(String[] args) {
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//        String rawPassword = "12345678";
+//        String encodedPassword = encoder.encode(rawPassword);
+//        System.out.println(encodedPassword);
+//    }
 }
